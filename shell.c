c7037fb88c598fc257743a104bfb3f50616d0831
Darren <darren2097@gmail.com>
2022-08-10 20:07:42 +0200

    Updated description

--- shell.c
+++ shell.c
@@ -7,14 +7,25 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-void execute(char** args)
+char **environ;
+
+/**
+ * execute - Executes the tokens
+ * @args: arguments
+ *
+ * Return: void
+ */
+
+void execute(char **args)
 {
+	char *env_args[] = { "PATH=/bin", "USER=me", NULL };
+
 	if (_strcmp(args[0], "cd") == 0)
 	{
 		chdir(args[1]);
 	}
 	else if ((_strcmp(args[0], "exit") == 0))
-	{		
+	{
 		write(STDOUT_FILENO, "Exiting shell...\n", 17);
 		exit(0);
 	}
@@ -22,7 +33,7 @@ void execute(char** args)
 	{
 		if (fork() == 0)
 		{
-			int stat_code = execvp(args[0], args);
+			int stat_code = execve(args[0], args, env_args);
 
 			if (stat_code == -1)
 			{
@@ -33,27 +44,34 @@ void execute(char** args)
 	}
 }
 
-char** parse(char* input)
-{	
+/**
+ * parse - Tokenization of input
+ * @input: input
+ *
+ * Return: tokens
+ */
+
+char **parse(char *input)
+{
 	int tokens_buf = 100;
-	char** tokens = malloc(tokens_buf * sizeof(char*));
+	char **tokens = malloc(tokens_buf * sizeof(char *));
 	int k = 0;
 
 	int token_buf = 1024;
-	char* token = malloc(token_buf * sizeof(char*));
+	char *token = malloc(token_buf * sizeof(char *));
 	int l = 0;
 
 	for (int i = 0; i < _strlen(input); i++)
 	{
 		char character = input[i];
-		
+
 		if (character == ' ')
 		{
 			if (k + 1 > tokens_buf)
 			{
 				tokens_buf += 100;
-				tokens = realloc(tokens, tokens_buf * sizeof(char*));
-			}																									
+				tokens = realloc(tokens, tokens_buf * sizeof(char *));
+			}
 			if (l + 1 >= token_buf)
 			{
 				token_buf += 1024;
@@ -62,7 +80,7 @@ char** parse(char* input)
 
 			tokens[k] = token;
 			k++;
-			token = malloc(token_buf * sizeof(char*));
+			token = malloc(token_buf * sizeof(char *));
 			l = 0;
 		}
 		else if (character == '\n')
@@ -70,7 +88,7 @@ char** parse(char* input)
 			if (k + 2 > tokens_buf)
 			{
 				tokens_buf += 100;
-				tokens = realloc(tokens, tokens_buf * sizeof(char*));
+				tokens = realloc(tokens, tokens_buf * sizeof(char *));
 			}
 
 			if (l + 1 > token_buf)
@@ -81,8 +99,7 @@ char** parse(char* input)
 			tokens[k] = token;
 			k++;
 			tokens[k] = NULL;
-			
-			break;			
+			break;
 		}
 		else
 		{
@@ -96,39 +113,50 @@ char** parse(char* input)
 			l++;
 		}
 	}
-	return tokens;
+	return (tokens);
 }
 
-char* read_line()
-{	
-	char* buf;
+/**
+ * read_line - Reads the input
+ *
+ * Return: buffer
+ */
+
+char *read_line(void)
+{
+	char *buf;
 	size_t bufsize = 100;
 	size_t line;
 
-	buf = (char*) malloc(bufsize * sizeof(char));
+	buf = (char *) malloc(bufsize * sizeof(char));
 	if (buf == NULL)
 	{
 		write(STDOUT_FILENO, "Error: Unable to allocate buffer", 32);
 		exit(1);
 	}
 	line = getline(&buf, &bufsize, stdin);
-	
-	return buf;
+	return (buf);
 }
 
-int main()
+/**
+ * main - Creates a simple shell
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
 {
 	write(STDOUT_FILENO, "Welcome to Simple_Shell!\n", 25);
 	_putchar('\n');
 	while (true)
 	{
-		char* input = read_line();
-		char** parsed = parse(input);			                             
-		
+		char *input = read_line();
+		char **parsed = parse(input);
+
 		execute(parsed);
 
 		free(input);
 		free(parsed);
-	}	
-	return 0;
+	}
+	return (0);
 }
